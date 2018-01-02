59e7bb73173f614ad523560a10c326036ceb1f1a
Adeel Ahmed <adeelahmed@Adeels-MacBook-Pro.local>
2018-01-02 17:16:11 -0600

    added packages

---
+++ app.js
@@ -0,0 +1,13 @@
+//packages
+var inquirer = require('inquirer');
+var randomWord = require('random-words');
+//get new word
+
+//array to store all guesses and seperate one to store correct
+
+//fxn to print correct letter to terminal if correct and underline if incorrect
+
+
+//
+
+
